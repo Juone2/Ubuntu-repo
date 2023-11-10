f0268d2d4f617e09f972f6db6a7d3f89b0380335
juwon <juwon2298@naver.com>
2023-11-10 12:28:14 +0900

    1110

--- example10/test.c
+++ example10/test.c
@@ -3,16 +3,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void int_handle(int signum);
+
+int num = 0;
+
+int main(void)
 {
 	static struct sigaction act;
 	act.sa_handler = int_handle;
 	sigfillset(&(act.sa_mask));
 	sigaction(SIGINT, &act, NULL);
+
+	while(1)
+	{
+		printf("i'm sleepy..\n");
+		sleep(1);
+		if(num >= 3) exit(0);
+	}
+
+	
 }
 
 void int_handle(int signum)
 {	
-	
-        printf("file write is end!\n", ++num);
+	{
+		FILE *fp;
+		fp = fopen("a.txt", "at");
+		printf("SIGINT:%d \n", signum);
+		fprintf(fp, "SIGINT: %d \n"), signum);
+		printf("int_handle called %d times \n", ++num);
+		fprintf(fp, "int_handle called %d times \n", num);
+	}
+	fclose(fp);
 } 
